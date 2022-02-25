992d9ad6d417e1185cfb58e7d8018c608ebc77cd
League Student <league@iMac-13.attlocal.net>
2022-02-24 18:27:28 -0800

    StringBuilder IP

--- src/_01_StringBuilder/_03_StringBuilder.java
+++ src/_01_StringBuilder/_03_StringBuilder.java
@@ -1,10 +1,17 @@
 package _01_StringBuilder;
 
+import java.util.Iterator;
 
 public class _03_StringBuilder {
+	static StringBuilder builder = new StringBuilder();
     
     public static String append(String str, char[] characters) {
-        return null;
+    	StringBuilder nsb = new StringBuilder(str);
+    	for (int i = 0; i < characters.length; i++) {
+    		nsb.append(characters[i]);
+    		System.out.println("append ......: " + nsb);
+		}
+        return nsb.toString();
     }
     
     public static String reverse(String str) {
